6dd6295e58c97fc6517923dad88990397c72ea79
Elephant5000 <pashau@mail.ru>
2020-06-09 19:42:18 +0300

    1.2.6.2. Каркас шахматной доски [#294118] Logic + BishopBlack

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -25,7 +25,17 @@ public class Logic {
         boolean rst = false;
         int index = this.findBy(source);
         if (index != -1) {
-            Cell[] steps = this.figures[index].way(source, dest);
+            Cell[] steps = null;
+            try {
+                steps = this.figures[index].way(source, dest);
+            } catch (IllegalStateException illegalStateException) {
+                return false;
+            }
+            for (Cell step : steps) {
+                if (findBy(step) != -1) {
+                    return false;
+                }
+            }
             if (steps.length > 0 && steps[steps.length - 1].equals(dest)) {
                 rst = true;
                 this.figures[index] = this.figures[index].copy(dest);
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,17 +4,27 @@ import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class LogicTest {
 
-    @Ignore
     @Test
-    public void move() {
+    public void whenLegalBishopMove() {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         boolean rsl = logic.move(Cell.C1, Cell.H6);
         assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void whenIllegalBishopMove() {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A1));
+        logic.add(new PawnBlack(Cell.B2));
+        boolean rsl = logic.move(Cell.A1, Cell.F6);
+        assertThat(rsl, is(false));
     }
 }
